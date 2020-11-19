712e9c739d9d21ecf015405afb995f3a557725a0
Buzmarev <grigory.buzmarev@tages.ru>
2020-11-20 00:19:39 +0300

    init static, templates with bootstrap. create elasticsearch and kibana services

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
 pip install --upgrade pip && pip install -r requirements.txt
+python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
+chown -R 1000:1000 ./
 python manage.py runserver 0.0.0.0:8000
